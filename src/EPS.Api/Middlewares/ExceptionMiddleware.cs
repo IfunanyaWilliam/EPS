1feb69f33a409c694260927277955ab99ef49935
Ifunanya <williamifunanya@gmail.com>
2025-03-25 17:15:18 +0100

    Add ExceptionMiddleware

--- src/EPS.Api/Middlewares/ExceptionMiddleware.cs
+++ src/EPS.Api/Middlewares/ExceptionMiddleware.cs
@@ -1,6 +1,52 @@
-﻿namespace EPS.Api.Middlewares
+﻿using EPS.Application.Common.DTO;
+using System.Text.Json;
+
+namespace EPS.Api.Middlewares
 {
     public class ExceptionMiddleware
     {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next,
+            ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            if (context.Response.HasStarted)
+            {
+                _logger.LogError("Response already started, skipping exception handling.");
+                return;
+            }
+
+            _logger.LogError(ex, "An exception occurred while processing the request.");
+
+            // Set status only if response hasn't started
+            context.Response.Clear();
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var errorResponse = ResponseBase<object>.Fail(
+                message: "An internal server error occurred.",
+                statusCode: StatusCodes.Status500InternalServerError);
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+        }
     }
 }
---
+++ src/EPS.Application/Common/DTO/ResponseBase.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json.Serialization;
+
+namespace EPS.Application.Common.DTO
+{
+    public class ResponseBase<T>
+    {
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+
+        [JsonPropertyName("isSuccess")]
+        public bool IsSuccess { get; set; }
+        public int StatusCode { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+
+        public ResponseBase<T> AddError(string error)
+        {
+            Errors?.Add(error);
+            return this;
+        }
+
+        public ResponseBase<T> AddError(List<string> error)
+        {
+            Errors?.AddRange(error);
+            return this;
+        }
+
+        public static ResponseBase<T> Success(string message, T data = default(T), int statusCode = StatusCodes.Status200OK)
+        {
+            return new ResponseBase<T>
+            {
+                Data = data,
+                Message = message,
+                StatusCode = statusCode,
+                IsSuccess = true
+            };
+        }
+
+        public static ResponseBase<T> Success(string message, int statusCode = StatusCodes.Status200OK)
+        {
+            return new ResponseBase<T>
+            {
+                Message = message,
+                StatusCode = statusCode,
+                IsSuccess = true
+            };
+        }
+
+        public static ResponseBase<T> Fail(string message, int statusCode = StatusCodes.Status400BadRequest)
+        {
+            return new ResponseBase<T>
+            {
+                Message = message,
+                StatusCode = statusCode,
+                IsSuccess = false
+            };
+        }
+
+        public static ResponseBase<T> Fail(string message, int statusCode, List<string> errors = null)
+        {
+            return new ResponseBase<T>
+            {
+                IsSuccess = false,
+                Message = message,
+                StatusCode = statusCode,
+                Errors = errors ?? new List<string> { message }
+            };
+        }
+    }
+}
